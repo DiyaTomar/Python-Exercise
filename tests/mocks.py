ffeba2a8aac7617ebab7dab9b17516cb811003a3
DiyaTomar <diyastomar@gmail.com>
2024-05-29 12:24:15 -0400

    Task 3.3.5 Add unit tests

--- tests/mocks.py
+++ tests/mocks.py
@@ -28,3 +28,15 @@ class MockDb(Db):
             return {"success": True, "text": "Hello"}
         else:
             raise ItemConflict(item_type.value, tenant_id, item_id)
+        
+    # update_item 
+    # similar to get_item since we want to ensure item to update exists in the db
+    @staticmethod
+    def update_item(item_type: ItemType, tenant_id: str, item_id: str):
+        if (
+            ItemKeys.get_keys(item_type=ItemType.ITEM, tenant_id=TENANT_ID, item_id=ITEM_ID).primary
+            == ItemKeys.get_keys(item_type=ItemType.ITEM, tenant_id=tenant_id, item_id=item_id).primary
+        ):
+            return {"success": True, "text": "some test text"}
+        else:
+            raise ItemNotFound(item_type.value, tenant_id, item_id)
