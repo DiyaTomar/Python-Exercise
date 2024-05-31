1254d35fb18c07738c18431d20af887537948b32
DiyaTomar <diyastomar@gmail.com>
2024-05-31 11:33:59 -0400

    Updated service.py to preserve modification info

--- python_exercise/service.py
+++ python_exercise/service.py
@@ -75,10 +75,14 @@ class Service:
         logger.info(f"Updating Item: {item_id}")
         now = datetime.datetime.utcnow().isoformat()  # getting the time that the data is modified
 
-        item["modification_info"] = {  # updating modification info
+        existing_item = self.database.get_item(item_type=ItemType.ITEM, tenant_id=self.tenant_id, item_id=item_id)
+
+        existing_item["modification_info"] = {  
             "last_modified_at": now,
             "last_modified_by": self.user_id,
         }
+
+        item["modification_info"] = existing_item["modification_info"] 
 
         # Updating item (passing in item info + the new data)
         self.database.update_item(item_type=ItemType.ITEM, tenant_id=self.tenant_id, item_id=item_id, item_data=item)
